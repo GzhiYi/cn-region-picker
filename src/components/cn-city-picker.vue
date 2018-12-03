4403da6dc9de09a9a3f65861118bdc65a01235a7
GzhiYi <gzy.guy@gmail.com>
2018-12-03 17:46:30 +0800

    添加基本组件结构

---
+++ src/components/cn-city-picker.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="picker">
+    picker content
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CnCityPicker',
+  data() {
+    return {
+
+    }
+  },
+  props: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import CnCityPicker from './cn-city-picker'
+
+CnCityPicker.install = Vue => Vue.component(CnCityPicker.name, CnCityPicker)
+
+export default CnCityPicker
